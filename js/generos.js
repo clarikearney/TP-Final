7e39e47a1a0b20192317b2d39db508de71654eb1
Mili <mscipioni@udesa.edu.ar>
2019-06-14 02:14:16 -0300

    aparecen las pelis en generos, faltan solucionar cosas

--- js/generos.js
+++ js/generos.js
@@ -1,50 +1,30 @@
 // quiero que al cliquear en mi genero la pagina web me redirija a las paginas de los generos que
 // selecciono el usuario!
-/*window.onload = function() {
-var imgPath = "https://image.tmdb.org/t/p/original"
-var url = "https://api.themoviedb.org/3/movie/upcoming?api_key=95b9e84c8317f917cebb3f232298f131&language=en-US&page=1"
-    fetch(url)
-      .then(function(respuesta) {
-        return respuesta.json()
-      })
-      .then(function(generos) {
-        var generos = informacion.genres
+window.addEventListener("load", function() {
+  var imgPath = "https://image.tmdb.org/t/p/original"
 
-        for (var i = 0; i < 6; i++) {
-          var id = generos[i].id // adaptar los puntos a las caract. de la API
-          var title = generos[i].title // adaptar los puntos a las caract. de la API
-          var imagenpelicula = generos[i].poster_path
-          document.querySelector(".lista-generos").innerHTML += "<li><img src=" + imgPath +imagenpelicula + " alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + title + "</h2></div></li>"
-        }
 
-      })
-      .catch(function(error) {
-        console.log("Error: " + error);
-      })
-
-}) */
-window.onload = function() {
-
-    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=95b9e84c8317f917cebb3f232298f131&language=en-US")
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=95b9e84c8317f917cebb3f232298f131&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=18") //  aca va la id de este html")
       .then(function(respuesta) {
         return respuesta.json()
       })
       .then(function(informacion) {
         console.log(informacion);
 
-        var generos = informacion.genres
+        var arrayDePeliculas = informacion.results
+        console.log(arrayDePeliculas);
 
-        for (var i = 0; i < 6; i++) {
-          var id = generos[i].id // adaptar los puntos a las caract. de la API
-          var title = generos[i].name // adaptar los puntos a las caract. de la API
+        for (var i = 0; i < 9; i++) {
+          var id = arrayDePeliculas[i].id
+          var title = arrayDePeliculas[i].title // adaptar los puntos a las caract. de la API
+          var imagenpeli= arrayDePeliculas[i].poster_path // adaptar los ptos a las caract. de la API
+          //document.querySelector(".ul-puntaje").innerHTML += "<article class='article-movie'><h2>" + title + "</h2> <img src=" + imgPath +imagenpelicula + "></article>"
 
-          document.querySelector("#desplegable").innerHTML += '<p> <a class="dropdown-menu" href=generos.html?id="' + id + '>' + title + '</a></p>'
-
+          document.querySelector(".pelis-genero").innerHTML += "<li><img src=" + imgPath + imagenpeli + " alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + title + "</h2></div></li>"
         }
-
       })
       .catch(function(error) {
         console.log("Error: " + error);
       })
 
-}
+})
