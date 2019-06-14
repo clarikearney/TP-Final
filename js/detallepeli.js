ee90077f618a45d636bcd4026d65fa8c4d804bc5
Mili <mscipioni@udesa.edu.ar>
2019-06-14 13:22:07 -0300

    estoy trabajando en la pagina detalle de pelis

--- js/detallepeli.js
+++ js/detallepeli.js
@@ -1,11 +1,11 @@
-// js que me tira el detalle de las peliculas con toda la info 
+// js que me tira el detalle de las peliculas con toda la info
 window.addEventListener("load", function() {
 
   var imgPath = "https://image.tmdb.org/t/p/original"
-  var queryString = new URLSearchParams(window.location.search).get("id")
+  var idDetalle = new URLSearchParams(window.location.search).get("id")
 
-    fetch("https://api.themoviedb.org/3/movie/" + idDetalle + "?api_key=95b9e84c8317f917cebb3f232298f131&language=en-US")
-      .then(function(respuesta) {
+    fetch("https://api.themoviedb.org/3/movie/" + idDetalle + "?api_key=95b9e84c8317f917cebb3f232298f131")
+    .then(function(respuesta) {
         return respuesta.json()
       })
       .then(function(informacion) {
@@ -23,11 +23,10 @@ window.addEventListener("load", function() {
         //  var fechaEstreno = arrrayDePeliculas[i].
         //  var trailer = arrayDePeliculas [i].
 
-
-        document.querySelector(".").innerHTML += "<li><img src=" + imgPath + imagenpeli + " alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + title + "</h2></div></li>"
-
+        document.querySelector(".detalle-peli").innerHTML += "<li><img src=" + imgPath + imagenpeli + " alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + title + "</h2></div></li>"
         }
       })
       .catch(function(error) {
         console.log("Error: " + error);
       })
+    })
