ca9d56a61bb4f8608872fa08b0d528ef035425f5
Clara Kearney <ckearney@udesa.edu.ar>
2019-06-14 11:47:45 -0300

    cambios en navbar (arregle prob logo) y buscador

--- js/buscador.js
+++ js/buscador.js
@@ -24,6 +24,7 @@ window.addEventListener("load", function() {
           //document.querySelector(".ul-puntaje").innerHTML += "<article class='article-movie'><h2>" + title + "</h2> <img src=" + imgPath +imagenpelicula + "></article>"
 
           document.querySelector(".detalle").innerHTML += "<li ><img src='" + imgPath +imagenpelicula + "' alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + title + "</h2></div></li>"
+
         }
       })
       .catch(function(error) {
