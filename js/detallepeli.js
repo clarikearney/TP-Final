5c563828cbba7d6f3ed3957b3887cbf22ce31c68
Mili <mscipioni@udesa.edu.ar>
2019-06-18 22:11:21 -0300

    cree la pagina de favoritos

--- js/detallepeli.js
+++ js/detallepeli.js
@@ -23,11 +23,8 @@ window.addEventListener("load", function() {
           for (var i=0; i < arrayDeGeneros.lenght; i++){
             generos += arrayDeGeneros[i].name + ", "
           }
-          //hasta aca llegue bien!
           var lenguajepeli = pelicula.original_language
-        //  var fechaEstreno = pelicula.
-        //  var trailer =
-
+         // hasta aca llegue bien!
         document.querySelector(".detalle-peli").innerHTML += "<li><h1>" + title + "</h1> <img src=" + imgPath + imagenpeli + " alt='' uk-cover><div class='uk-position-center uk-position-small uk-text-center uk-light'><h2 class='uk-margin-remove'>" + "<p> <a href=generos.html" + generos + "</a></p></li>"
 
       })
