8e73b692785a0a15b8b26d629a8ddbd34a0efc1f
Clara Kearney <ckearney@udesa.edu.ar>
2019-06-21 03:52:24 -0300

    carrusel peliculas recomendadas y cambios en js detallepelis y otros varios

--- js/detallepeli.js
+++ js/detallepeli.js
@@ -109,7 +109,7 @@ fetch("https://api.themoviedb.org/3/movie/" + idDePelicula + "/recommendations?a
       var id = arrayDePeliculas[i].id
       var title = arrayDePeliculas[i].title
       var imagenpelicula = arrayDePeliculas[i].poster_path
-      li = ''
+      var li = ''
       li += '<li>'
       li += '<a href="detallepeli.html?idDePelicula=' + id +'"'
       // li += '<li>'
@@ -119,6 +119,7 @@ fetch("https://api.themoviedb.org/3/movie/" + idDePelicula + "/recommendations?a
       li += '</a>'
       li += '</li>'
       var ul = document.querySelector(".recomendaciones")
+
       ul.innerHTML += li
   }
 })
